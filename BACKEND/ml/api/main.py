bc4b493ccb223ddebe35a4ec2572c8ed564fcb96
Dhruvi432 <dhruvisavliya478@gmail.com>
2026-09-25 01:17:21 +0530

    Fix CORS configuration

--- BACKEND/ml/api/main.py
+++ BACKEND/ml/api/main.py
@@ -9,10 +9,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://vehicle-insurance-fraud-ml-2.onrender.com",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        # "https://vehicle-insurance-fraud-ml-1.onrender.com",
-        "https://vehicle-insurance-fraud-ml-2.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
