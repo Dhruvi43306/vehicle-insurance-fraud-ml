f2f4a8a23a8736206f5b30ab81333b6ed4ad17f9
Dhruvi432 <dhruvisavliya478@gmail.com>
2026-09-25 01:09:14 +0530

    Fix CORS for deployed frontend

--- BACKEND/ml/api/main.py
+++ BACKEND/ml/api/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         # "https://vehicle-insurance-fraud-ml-1.onrender.com",
-        "https://vehicle-insurance-fraud-frontend.onrender.com"
+        "https://vehicle-insurance-fraud-ml-2.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
