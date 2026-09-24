8fa70234c617d96b67094e051a3dabdb03ecb61f
Dhruvi432 <dhruvisavliya478@gmail.com>
2026-09-25 00:10:48 +0530

    Connect frontend to deployed ML API

--- FRONTEND/my-react-app/src/pages/Prediction.jsx
+++ FRONTEND/my-react-app/src/pages/Prediction.jsx
@@ -1058,7 +1058,7 @@ export default function Prediction() {
     console.log(payload);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://vehicle-insurance-fraud-ml-1.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
